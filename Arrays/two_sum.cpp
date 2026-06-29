3b45f6dd08753b44e0e06006b3b3faf43aee7181
MD UMAR RAHMANI <urahmani21@gmail.com>
2026-06-29 23:00:23 +0530

    Solved LeetCode #1 Two Sum (Brute Force)

---
+++ Arrays/two_sum.cpp
@@ -0,0 +1,51 @@
+// 1. Two Sum
+// class Solution {
+// public:
+//     vector<int> twoSum(vector<int>& nums, int target) {
+//         for(int i=0;i<nums.size();i++)
+//         {
+//             for(int j=i+1;j<nums.size();j++)
+//             {
+//                 if(nums[i] + nums[j] == target)
+//                 {
+//                     return {i,j};
+//                 }
+
+//             }
+//         }
+//         return {};
+//     }
+// };
+
+// Leetcode question - two sum - VS code 
+#include<iostream>
+#include<vector>
+using namespace std;
+int main()
+    {   int n;
+        cout<<"Enter size of array: ";
+        cin>>n;
+        int target;
+        cout<<"Enter target: ";
+        cin>>target;
+        vector<int> nums(n);
+        for(int i=0;i<nums.size();i++)
+        {
+            cin>>nums[i];
+        }
+
+        for(int i=0;i<nums.size();i++)
+        {
+            for(int j=i+1;j<nums.size();j++)
+            {
+                if(nums[i]+nums[j]==target)
+                {
+                    cout<<i<<" "<<j;
+                    
+                }
+                
+            }
+        }
+
+
+    }
