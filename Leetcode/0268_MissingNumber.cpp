e540e197d2eb4b235d5561361112c91e66ea4a81
MD UMAR RAHMANI <urahmani21@gmail.com>
2026-07-14 23:39:03 +0530

    Solved LeetCode 268

---
+++ Leetcode/0268_MissingNumber.cpp
@@ -0,0 +1,18 @@
+// class Solution {
+// public:
+//     int missingNumber(vector<int>& nums) {
+//         int n=nums.size();
+//         // expected sum 
+//         long long sum=0;
+//         long long expectedsum=((n) * (n+1))/2;
+//         for(int i=0;i<n;i++)
+//         {
+//             sum=sum+nums[i];
+//         }
+//         return expectedsum-sum;
+
+
+
+
+//     }
+// };
