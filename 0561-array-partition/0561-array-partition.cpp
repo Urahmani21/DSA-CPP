455b45e10d66349382daf394113fa82ea92d8f72
MD UMAR RAHMANI <urahmani21@gmail.com>
2026-07-16 19:22:57 +0530

    0561-array-partition

---
+++ 0561-array-partition/0561-array-partition.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int arrayPairSum(vector<int>& nums) {
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+        int sum=0;
+        for(int i=0;i<n;i=i+2)
+        {   // nums 0 = nums1
+        
+            sum=sum+nums[i];
+        }
+        return sum;
+    }
+};
