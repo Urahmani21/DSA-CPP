247670cba59f75e435a5bc42d9db24d57f445b75
MD UMAR RAHMANI <urahmani21@gmail.com>
2026-08-14 01:12:31 +0530

    0268-missing-number

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,21 +1,18 @@
-// class Solution {
-// public:
-//     int missingNumber(vector<int>& nums) {
-//         int n=nums.size();
-//         // expected sum 
-//         long long sum=0;
-//         long long expectedsum=((n) * (n+1))/2;
-//         for(int i=0;i<n;i++)
-//         {
-//             sum=sum+nums[i];
-//         }
-//         return expectedsum-sum;
-
-
-
-
-//     }
-// };
+// METHOD 1
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n=nums.size();
+        // expected sum 
+        long long sum=0;
+        long long expectedsum=((n) * (n+1))/2;
+        for(int i=0;i<n;i++)
+        {
+            sum=sum+nums[i];
+        }
+        return expectedsum-sum;
+    }
+};
 
 // method 2 
 
@@ -35,29 +32,29 @@
 //     }
 // };
 
-// method 3
+// // method 3
 
-class Solution {
-public:
-    int missingNumber(vector<int>& nums) {
-       int n = nums.size();
-       for(int i=0;i<=n;i++)
-       {
-        bool flag = false;
+// class Solution {
+// public:
+//     int missingNumber(vector<int>& nums) {
+//        int n = nums.size();
+//        for(int i=0;i<=n;i++)
+//        {
+//         bool flag = false;
 
-        for(int j=0;j<n;j++)
-        {
-            if(nums[j] == i )
-            {
-                flag = true;
-                break;
-            }
-        }
-        if(flag == false)
-        {
-            return i;
-        }
-       }
-       return 0;
-    }
-};
+//         for(int j=0;j<n;j++)
+//         {
+//             if(nums[j] == i )
+//             {
+//                 flag = true;
+//                 break;
+//             }
+//         }
+//         if(flag == false)
+//         {
+//             return i;
+//         }
+//        }
+//        return 0;
+//     }
+// };
